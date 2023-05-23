1ba6460b1bde47a61e99b56cda8f9c926b8d38b0
Mark <mark@remarkablemark.org>
2023-05-22 21:03:43 -0400

    feat(functions): add v1 endpoint

---
+++ functions/v1.ts
@@ -0,0 +1,25 @@
+/**
+ * @see {@link https://developers.cloudflare.com/workers/examples/cors-header-proxy/}
+ */
+export const onRequest: PagesFunction = async (context) => {
+  const url = new URL(context.request.url);
+  const targetUrl = url.searchParams.get('url');
+
+  // Rewrite request to point to target URL.
+  // This also makes the request mutable so you can add the correct
+  // Origin header to make the API server think that this request is not cross-site.
+  context.request = new Request(targetUrl, context.request);
+  context.request.headers.set('Origin', new URL(targetUrl).origin);
+
+  // Recreate the response so you can modify the headers
+  let response = await fetch(context.request);
+  response = new Response(response.body, response);
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', '*');
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append('Vary', 'Origin');
+
+  return response;
+};
