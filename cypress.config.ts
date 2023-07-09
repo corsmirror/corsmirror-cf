977db092483a9cef801ae88c12c0e6c9c7887cf0
Mark <mark@remarkablemark.org>
2023-07-08 20:40:06 -0400

    test(cypress): set up cypress-cucumber-steps

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'cypress';
+import createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    baseUrl: 'http://127.0.0.1:8788',
+    specPattern: 'cypress/e2e/**/*.feature',
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        'file:preprocessor',
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      );
+      return config;
     },
   },
 });
