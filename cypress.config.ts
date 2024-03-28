d424e84e8dbc20901202909b402fbc44567f3031
Mark <mark@remarkablemark.org>
2024-03-28 00:11:08 -0400

    test(cypress): fix baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'cypress';
 import createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+// @ts-expect-error module exists
 import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8788',
+    baseUrl: 'http://127.0.0.1:8787',
     specPattern: 'cypress/e2e/**/*.feature',
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
